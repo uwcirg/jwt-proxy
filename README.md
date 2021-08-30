139cdf90befdfaf83f52368e0ead7a42eecc370c
Ivan Cvitkovic <ivanc@uw.edu>
2021-08-30 10:35:57 -0700

    Initial Work (#1)
    * Initial commit
    
    * More
    
    * Add JWT
    
    * Add config
    
    * Add requests
    
    * Add route to authenticate JWTs
    
    * Add README
    
    * Add continuous delivery
    
    * Remove debugging config
    
    * Fixup header method
    
    * Fixup config
    
    * Fixup path issue
    
    * Cleanup JSON returns
    
    * Add more methods
    
    * Fixup syntax error
    
    * Passthrough request method
    
    * Add config for path whitelist, handle non-JSON responses

---
+++ README.md
@@ -0,0 +1,2 @@
+# JSON Web Token Auth Proxy
+Validates JWTs and forwards requests to an upstream server
---
+++ jwt_proxy/api.py
@@ -0,0 +1,88 @@
+from flask import Blueprint, abort, current_app, jsonify, request
+import jwt
+import requests
+import json
+
+blueprint = Blueprint('auth', __name__)
+SUPPORTED_METHODS = ('GET', 'POST', 'PUT', 'DELETE')
+
+
+def proxy_request(req, upstream_url):
+    """Forward request to given url"""
+    response = requests.request(
+        method=req.method,
+        url=upstream_url,
+        headers=req.headers,
+        params=req.args,
+        json=req.json,
+    )
+    try:
+        return response.json()
+    except json.decoder.JSONDecodeError:
+        return response.text
+
+@blueprint.route('/', defaults={'relative_path': ''}, methods=SUPPORTED_METHODS)
+@blueprint.route('/<path:relative_path>', methods=SUPPORTED_METHODS)
+def validate_jwt(relative_path):
+    """Validate JWT and pass to upstream server"""
+    if f"/{relative_path}" in current_app.config["PATH_WHITELIST"]:
+        response_content = proxy_request(
+            req=request,
+            upstream_url=f"{current_app.config['UPSTREAM_SERVER']}/{relative_path}"
+        )
+        return response_content
+
+    token = request.headers.get("authorization", "").split("Bearer ")[-1]
+    if not token:
+        return jsonify(message="token missing"), 400
+
+    jwks_client = jwt.PyJWKClient(current_app.config["JWKS_URL"])
+    signing_key = jwks_client.get_signing_key_from_jwt(token)
+
+    try:
+        decoded_token = jwt.decode(
+            jwt=token,
+            # TODO cache public key in redis
+            key=signing_key.key,
+            algorithms=("RS256"),
+            audience=("account"),
+        )
+    except jwt.exceptions.ExpiredSignatureError:
+        return jsonify(message="token expired"), 401
+
+    response_content = proxy_request(
+        req=request,
+        upstream_url=f"{current_app.config['UPSTREAM_SERVER']}/{relative_path}"
+    )
+    return response_content
+
+
+@blueprint.route('/settings', defaults={'config_key': None})
+@blueprint.route('/settings/<string:config_key>')
+def config_settings(config_key):
+    """Non-secret application settings"""
+
+    # workaround no JSON representation for datetime.timedelta
+    class CustomJSONEncoder(flask.json.JSONEncoder):
+        def default(self, obj):
+            return str(obj)
+    current_app.json_encoder = CustomJSONEncoder
+
+    # return selective keys - not all can be be viewed by users, e.g.secret key
+    blacklist = ('SECRET', 'KEY')
+
+    if config_key:
+        key = config_key.upper()
+        for pattern in blacklist:
+            if pattern in key:
+                abort(400, f"Configuration key {key} not available")
+        return jsonify({key: current_app.config.get(key)})
+
+    results = {}
+    for key in current_app.config:
+        matches = any(pattern for pattern in blacklist if pattern in key)
+        if matches:
+            continue
+        results[key] = current_app.config.get(key)
+
+    return jsonify(results)
