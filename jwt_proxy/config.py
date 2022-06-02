3e486993a9f59a6d288f4e5bcd19fe697581246a
Paul F Bugni <pbugni@uw.edu>
2022-06-01 22:43:16 -0700

    Added TODO to capture review comment

--- jwt_proxy/config.py
+++ jwt_proxy/config.py
@@ -4,8 +4,10 @@ Use environment variable to override
 """
 import os
 
-
 # $KEYCLOAK_FQDN/auth/realms/$REALM/protocol/openid-connect/certs
+
+# TODO replace individual OIDC strings by parsing well-known from keycloak:
+# https://keycloak.acc.dev.cosri.cirg.washington.edu/auth/realms/fEMR/.well-known/openid-configuration
 OIDC_AUTHORIZE_URL = os.getenv("OIDC_AUTHORIZE_URL")
 OIDC_TOKEN_URI = os.getenv("OIDC_TOKEN_URI")
 OIDC_TOKEN_INTROSPECTION_URI = os.getenv("OIDC_TOKEN_INTROSPECTION_URI")
