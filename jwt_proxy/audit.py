06189ab043a7fc3de62ed7abb0beb9785ef1f1d7
Paul F Bugni <pbugni@u.washington.edu>
2023-01-23 17:30:01 -0800

    Enhance parsing of ResourceType and id from variety of FHIR url patterns

--- jwt_proxy/audit.py
+++ jwt_proxy/audit.py
@@ -4,6 +4,7 @@ functions to simplify adding context and extra data to log messages destined for
 """
 from flask import current_app, has_app_context
 import logging
+from urllib.parse import urlparse
 
 from jwt_proxy.logserverhandler import LogServerHandler
 
@@ -34,13 +35,27 @@ def audit_entry(message, level="info", extra=None):
     log_at_level(message, extra=extra)
 
 
+def deets_from_url(url, resource_type, id):
+    """Given url, and best guess at resource_type and id, return best guess"""
+    if resource_type and id:
+        return resource_type, id
+
+    # url: UPSTREAM_SERVER/fhir/ResourceType/<id or params>
+    parsed = urlparse(url)
+    if not parsed.path.startswith("/fhir/"):
+        audit_entry(f"Unexpected fhir path: {url} can't parse", level="error")
+    items = parsed.path.split('/')
+    resource_type = resource_type or items[2]
+    id = id or items[3] if len(items) > 3 else None
+    return resource_type, id
+
+
 def audit_HAPI_change(
     user_info, method, params=None, resource=None, resource_type=None, resource_id=None, url=None
 ):
     rt = resource_type or resource and resource.get("resourceType")
-    if not rt:
-        rt = url.split("/")[-1]
-    id = resource_id or resource and resource.get("_id", "")
+    id = resource_id or resource and resource.get("_id")
+    rt, id = deets_from_url(url, rt, id)
     msg = f"{method} {rt}/{id}" if id else f"{method} {rt}"
     extra = {
         "event_version": EVENT_VERSION,
@@ -54,4 +69,5 @@ def audit_HAPI_change(
 
     if params:
         extra["params"] = params
+    # extra["url"] = url  # helpful in debugging
     audit_entry(message=msg, extra=extra)
