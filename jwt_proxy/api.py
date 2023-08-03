1ce86c677c029c7994fc22475ce50e663202bd6b
pbugni <pbugni@u.washington.edu>
2023-08-03 09:00:47 -0700

    Update jwt_proxy/api.py
    include closing comma for cleaner diffs down the road
    
    Co-authored-by: Ivan Cvitkovic <ivanc@uw.edu>

--- jwt_proxy/api.py
+++ jwt_proxy/api.py
@@ -17,7 +17,7 @@ def proxy_request(req, upstream_url, user_info=None):
         headers=req.headers,
         params=req.args,
         json=req.json,
-        data=req.data
+        data=req.data,
     )
     try:
         result = response.json()
