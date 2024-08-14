2a5655f976f004b3d620b948ea9de466948f607d
Daniil <94884910+Filienko@users.noreply.github.com>
2024-08-13 17:43:21 -0700

    Removing json

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,5 @@
 import unittest
-from flask import Flask, json
+from flask import Flask
 from jwt_proxy.api import blueprint, proxy_request
 import json
 from unittest.mock import patch, MagicMock
@@ -13,7 +13,7 @@ class TestAuthBlueprint(unittest.TestCase):
         self.app.config['TESTING'] = True
         self.app.config['UPSTREAM_SERVER'] = 'http://example.com'
         self.app.config['JWKS_URL'] = 'http://jwks.example.com'
-        self.app.config['PATH_WHITELIST'] = ['/whitelisted']
+        self.app.config['PATH_WHITELIST'] = ['/whitelisted']  # Adding the whitelisted path
         self.app.config['OIDC_AUTHORIZE_URL'] = 'http://authorize.example.com'
         self.app.config['OIDC_TOKEN_URI'] = 'http://token.example.com'
         self.app.config['OIDC_TOKEN_INTROSPECTION_URI'] = 'http://introspection.example.com'
