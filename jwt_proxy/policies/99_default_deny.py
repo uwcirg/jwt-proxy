d01dc6e9813267cd97dde1855ec09fc3add53aad
Ivan Cvitkovic <ivanc@uw.edu>
2025-11-03 13:18:53 -0800

    wip

---
+++ jwt_proxy/policies/99_default_deny.py
@@ -0,0 +1,8 @@
+def evaluate(request, user_info):
+    """Default deny policy - denies all requests that don't match earlier rules.
+    
+    This policy should be loaded last (via filename ordering) to act as a catch-all
+    default deny for any request not explicitly allowed by previous policies.
+    """
+    return ("deny", "Request denied by default policy - no matching rule found")
+
